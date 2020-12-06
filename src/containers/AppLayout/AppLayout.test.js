ad1184105f30508f2415bc85c6fefcb8aabf0c8e
martinpham97 <tutin2678@gmail.com>
2020-12-06 20:44:42 +1100

    Add snapshot tests

--- src/containers/AppLayout/AppLayout.test.js
+++ src/containers/AppLayout/AppLayout.test.js
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { axe } from "jest-axe";
 
 import { renderWithWrappers } from "utils/testing";
@@ -16,6 +16,34 @@ describe("<AppLayout />", () => {
     );
 
     expect(screen.queryByText("Test Child")).not.toBeNull();
+  });
+
+  it("should toggle sidebar on clicking sidebar button", async () => {
+    const { container } = renderWithWrappers(<AppLayout />);
+
+    fireEvent.click(screen.queryAllByLabelText(/toggle-sidebar/i)[0]);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should toggle light/dark mode on clicking toggle dark mode button", async () => {
+    const { container } = renderWithWrappers(<AppLayout />);
+
+    fireEvent.click(screen.getByLabelText(/toggle-dark/i));
+    fireEvent.click(screen.getByLabelText(/toggle-dark/i));
+    fireEvent.click(screen.getByLabelText(/toggle-dark/i));
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should change language when language is selected", async () => {
+    const { container } = renderWithWrappers(<AppLayout />);
+
+    fireEvent.click(screen.getByLabelText(/change-language/i));
+
+    fireEvent.click(screen.queryAllByRole("menuitem")[1]);
+
+    expect(container).toMatchSnapshot();
   });
 
   it("should be accessible", async () => {
