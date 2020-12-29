a4a74a6c618b88abe7398b5ff273552ac41218b4
martinpham97 <tutin2678@gmail.com>
2020-12-29 14:34:19 +1100

    Add Google Analytics

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,11 +7,13 @@ import CourseListPage from "containers/CourseListPage/CourseListPage";
 import CourseDetailPage from "containers/CourseDetailPage/CourseDetailPage";
 import SettingsPage from "containers/SettingsPage/SettingsPage";
 import AboutPage from "containers/AboutPage/AboutPage";
+import GA from "containers/GoogleAnalytics/GoogleAnalytics";
 
 export const App = () => (
   <Router>
     <AppLayout>
       <Switch>
+        {GA.init() && <GA.RouteTracker />}
         <Route path="/" exact>
           <HomePage />
         </Route>
