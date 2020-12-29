81391a0e13744b9d974a4259dbb2dcb58a0bccf1
martinpham97 <tutin2678@gmail.com>
2020-12-29 14:57:56 +1100

    Use hooks for GA

---
+++ src/analytics.js
@@ -0,0 +1,27 @@
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-39JLJKLNYH";
+
+function init() {
+  // Enable debug mode on the local development environment
+  const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+  const isTest = process.env.NODE_ENV === "test";
+  ReactGA.initialize(TRACKING_ID, { debug: isDev, testMode: isTest });
+}
+
+function sendEvent(payload) {
+  ReactGA.event(payload);
+}
+
+function sendPageview(path) {
+  ReactGA.set({ page: path });
+  ReactGA.pageview(path);
+}
+
+const GA = {
+  init,
+  sendEvent,
+  sendPageview,
+};
+
+export default GA;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,32 +7,40 @@ import CourseListPage from "containers/CourseListPage/CourseListPage";
 import CourseDetailPage from "containers/CourseDetailPage/CourseDetailPage";
 import SettingsPage from "containers/SettingsPage/SettingsPage";
 import AboutPage from "containers/AboutPage/AboutPage";
-import GA from "containers/GoogleAnalytics/GoogleAnalytics";
+
+import useGoogleAnalytics from "hooks/useGoogleAnalytics";
+
+const Routes = () => {
+  useGoogleAnalytics();
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route path="/courses" exact>
+        <CourseListPage />
+      </Route>
+      <Route path="/courses/:courseId">
+        <CourseDetailPage />
+      </Route>
+      <Route path="/settings">
+        <SettingsPage />
+      </Route>
+      <Route path="/about">
+        <AboutPage />
+      </Route>
+      <Route path="*">
+        <NotFoundPage />
+      </Route>
+    </Switch>
+  );
+};
 
 export const App = () => (
   <Router>
     <AppLayout>
-      <Switch>
-        {GA.init() && <GA.RouteTracker />}
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/courses" exact>
-          <CourseListPage />
-        </Route>
-        <Route path="/courses/:courseId">
-          <CourseDetailPage />
-        </Route>
-        <Route path="/settings">
-          <SettingsPage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
-        </Route>
-        <Route path="*">
-          <NotFoundPage />
-        </Route>
-      </Switch>
+      <Routes />
     </AppLayout>
   </Router>
 );
